f70398eb4dd889cb7848956ebdd9228ae00dc426
lggyx <19546328912@163.com>
2025-09-24 11:19:15 +0800

    Mybatis-Plus核心功能：条件构造器 QueryWrapper：基础的增删改查 UpdateWrapper：复杂需求的更新 LambdaQueryWrapper\LambdaUpdateWrapper：反射技术获取字段名,已经废弃（貌似） 自定义SQL：MybatisPlus提供了自定义SQL功能，可以让我们利用Wrapper生成查询条件，再结合Mapper.xml编写SQL Service接口

---
+++ mp-demo/src/main/java/com/itheima/mp/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.itheima.mp.controller;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.itheima.mp.domain.dto.UserFormDTO;
+import com.itheima.mp.domain.po.User;
+import com.itheima.mp.domain.vo.UserVO;
+import com.itheima.mp.service.IUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(tags = "用户管理接口")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("users")
+public class UserController {
+    @Autowired
+    IUserService userService;
+
+    @PostMapping
+    @ApiOperation("新增用户")
+    public void saveUser(@RequestBody UserFormDTO userFormDTO){
+        // 1.转换DTO为PO
+        User user = BeanUtil.copyProperties(userFormDTO, User.class);
+        // 2.新增
+        userService.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation("删除用户")
+    public void removeUserById(@PathVariable("id") Long userId){
+        userService.removeById(userId);
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询用户")
+    public UserVO queryUserById(@PathVariable("id") Long userId){
+        // 1.查询用户
+        User user = userService.getById(userId);
+        // 2.处理vo
+        return BeanUtil.copyProperties(user, UserVO.class);
+    }
+
+    @GetMapping
+    @ApiOperation("根据id集合查询用户")
+    public List<UserVO> queryUserByIds(@RequestParam("ids") List<Long> ids){
+        // 1.查询用户
+        List<User> users = userService.listByIds(ids);
+        // 2.处理vo
+        return BeanUtil.copyToList(users, UserVO.class);
+    }
+}
