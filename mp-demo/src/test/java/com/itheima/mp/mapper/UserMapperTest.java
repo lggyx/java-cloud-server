83d0ebd6f4084ec878a677c9347fda281c391e38
lggyx <19546328912@163.com>
2025-09-24 10:47:52 +0800

    Mybatis-Plus核心功能：条件构造器 QueryWrapper：基础的增删改查 UpdateWrapper：复杂需求的更新

--- mp-demo/src/test/java/com/itheima/mp/mapper/UserMapperTest.java
+++ mp-demo/src/test/java/com/itheima/mp/mapper/UserMapperTest.java
@@ -1,6 +1,7 @@
 package com.itheima.mp.mapper;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.itheima.mp.domain.po.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -82,5 +83,16 @@ class UserMapperTest {
         User user = new User();
         user.setBalance(2000);
         userMapper.update(user, wrapper);
+    }
+    @Test
+    void testUpdateWrapper() {
+        List<Long> ids = List.of(1L, 2L, 4L);
+        // 1.生成SQL
+        UpdateWrapper<User> wrapper = new UpdateWrapper<User>()
+                .setSql("balance = balance - 200") // SET balance = balance - 200
+                .in("id", ids); // WHERE id in (1, 2, 4)
+        // 2.更新，注意第一个参数可以给null，也就是不填更新字段和数据，
+        // 而是基于UpdateWrapper中的setSQL来更新
+        userMapper.update(null, wrapper);
     }
 }
