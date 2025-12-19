973d2f1dfbbe3ad8ebf34c642b3d866f4c019c88
lggyx <19546328912@163.com>
2025-12-19 22:30:21 +0800

    微服务item模块拆分成功

--- item-service/src/main/java/com/hmall/item/mapper/ItemMapper.java
+++ item-service/src/main/java/com/hmall/item/mapper/ItemMapper.java
@@ -1,8 +1,8 @@
-package com.hmall.mapper;
+package com.hmall.item.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.hmall.domain.dto.OrderDetailDTO;
-import com.hmall.domain.po.Item;
+import com.hmall.item.domain.dto.OrderDetailDTO;
+import com.hmall.item.domain.po.Item;
 import org.apache.ibatis.annotations.Update;
 
 /**
