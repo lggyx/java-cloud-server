9f31851cb53529337fc27bc6ce29c58e00d772ac
lggyx <19546328912@163.com>
2025-12-20 01:32:54 +0800

    完成支付模块迁移

---
+++ hm-api/src/main/java/com/hmall/api/dto/PayOrderFormDTO.java
@@ -0,0 +1,17 @@
+package com.hmall.api.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@Builder
+@ApiModel(description = "支付确认表单实体")
+public class PayOrderFormDTO {
+    @ApiModelProperty("支付订单id不能为空")
+    private Long id;
+    @ApiModelProperty("支付密码")
+    private String pw;
+}
